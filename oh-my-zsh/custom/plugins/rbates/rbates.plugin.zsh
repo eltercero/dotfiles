322f7d69e808546ec8d7e14a74a49f342741d4ff
Víctor <victor@cool-tabs.com>
2019-08-20 08:58:34 +0200

    Updating gitconfig with new command aliases, plugins and gemrc

--- oh-my-zsh/custom/plugins/rbates/rbates.plugin.zsh
+++ oh-my-zsh/custom/plugins/rbates/rbates.plugin.zsh
@@ -11,6 +11,14 @@ rails_migrate_down() {
   [[ -n "$version" ]] && rails db:migrate:down VERSION="$version"
 }
 
+fbr() {
+  git fetch
+  local branches branch
+  branches=$(git branch -a) &&
+  branch=$(echo "$branches" | fzf +s +m -e) &&
+  git checkout $(echo "$branch" | sed "s:.* remotes/origin/::" | sed "s:.* ::")
+}
+
 # autocorrect is more annoying than helpful
 unsetopt correct_all
 
